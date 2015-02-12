27c7ef75499d2cb97c9099555861d7414a8b0d59
withat <0x6d48@gmail.com>
2015-02-12 09:35:31 +0100

    Re-structured plugin wine

---
+++ src/plugins/wine/models/__init__.py
@@ -0,0 +1,7 @@
+from .gattung import Gattung
+from .jahrgang import Jahrgang
+from .land import Land
+from .rebsorte import Rebsorte
+from .region import Region
+from .wein import Wein
+from .weingut import Weingut
